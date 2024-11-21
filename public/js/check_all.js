ab3f85d663040517f39d4ee02c7aa4d46d166a96
Ishodayo <ishodajo@gmail.com>
2024-11-22 02:04:22 +0700

    xoa file thua

--- public/js/check_all.js
+++
@@ -1,7 +0,0 @@
-src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-
-$(document).ready(function(){
-    $("#table #check_all").click(function(){
-        $("#table input[type=checkbox]").prop('checked',this.checked);
-    });
-});
