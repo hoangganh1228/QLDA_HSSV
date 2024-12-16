dc2b86cb70595c234daa3ec8d4b2ae90f1fea73a
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-12-16 22:57:15 +0700

    Chức năng thêm, sửa, xóa, quản lý học phí

--- app/controllers/admin/Tuition.php
+++ app/controllers/admin/Tuition.php
@@ -1,10 +1,10 @@
 <?php
-class Users extends Controller
+class Tuition extends Controller
 {
     private $data, $model;
 
     function __construct() {
-        $this->model = $this->model('admin/UsersModel'); 
+        $this->model = $this->model('admin/TuitionModel'); 
         if (!$this->model) {
             die("Không thể tải model users.model.");
         }
@@ -12,65 +12,67 @@ class Users extends Controller
     
     function index()
     {
-        $this->list_user();
+        $this->list_tuition();
     }
 
-    function list_user() {
+    function list_tuition() {
         $search = isGet() ? (!empty($_GET['search']) ? $_GET['search'] : '') : '';
-        $result = $this->model->getAllUsers($search);
+        $result = $this->model->getAllTuitions($search);
         if ($result === false) {
             die("Lỗi truy vấn cơ sở dữ liệu.");
         }
 
         $this->data['table'] = $result; 
-        $this->view('/admin/users/index', [
+        $this->view('/admin/tuition/index', [
             'table' => $this->data['table']
         ]);
     }
 
     public function create() {
         if (isPost()) {
-            $filteredPost = filter();
-            
-
-            if (!$this->model->isDuplicateKhoaId($filteredPost['user_id'])) {
-                $this->model->addUser($filteredPost);
-                echo "<script>alert('Thêm người dùng thành công!')</script>";
-                echo "<script>window.location.href = '/QLDA_HSSV/admin/users/index'</script>";
-            } else {
-                echo "<script>alert('Thêm ngành thất bại, trùng mã ngành!')</script>";
-            }
+          $filteredPost = filter();
+          if (!$this->model->isDuplicateTuitionId($filteredPost['user_id'])) {
+            $this->model->addTuition($filteredPost);
+            echo "<script>alert('Thêm học phí thành công!')</script>";
+            echo "<script>window.location.href = '/QLDA_HSSV/admin/tuition/index'</script>";
+          } else {
+            echo "<script>alert('Thêm ngành thất bại, trùng mã ngành!')</script>";
+          }
         }
-        $user = $this->model->getAllUsers();
-        $this->view('/admin/users/create', ['user' => $user]);
+        $tuition = $this->model->getAllTuitions();
+        $departments = $this->model->getDepartment();
+        $this->view('/admin/tuition/create', [
+          'tuition' => $tuition,
+          'departments' => $departments
+        ]);
     }
 
     public function edit($id = '') {
         if (isPost()) {
             $filteredPost = filter(); 
-            $this->model->updateUser($id, $filteredPost); 
-            echo "<script>alert('Sửa người dùng thành công')</script>";
-            echo "<script>window.location.href = '/QLDA_HSSV/admin/users'</script>";
+            $this->model->updateTuition($id, $filteredPost); 
+            echo "<script>alert('Sửa học phí thành công')</script>";
+            echo "<script>window.location.href = '/QLDA_HSSV/admin/tuition'</script>";
         } else {
-            $result = $this->model->getUserById($id); 
+            $result = $this->model->getTuitionById($id); 
             if (!$result) {
-                die("Không tìm thấy người dùng.");
+              die("Không tìm thấy học phí.");
             }
-            $users = $this->model->getAllUsers();
-            $this->view('/admin/users/edit', [
-                'data' => $result,
-                'users' => $users
+            $departments = $this->model->getDepartment();
+            $this->view('/admin/tuition/edit', [
+              'data' => $result,
+              'departments' => $departments
             ]);
         }
     }
 
     public function delete($id = '') {
-        if ($this->model->deleteUser($id)) {
+        if ($this->model->deleteTuition($id)) {
             echo "<script>alert('Xóa thành công')</script>";
         } else {
             echo "<script>alert('Xóa thất bại')</script>";
         }
-        echo "<script>window.location.href = '/QLDA_HSSV/admin/users'</script>";
+        echo "<script>window.location.href = '/QLDA_HSSV/admin/tuition'</script>";
     }
 
     public function login() {
