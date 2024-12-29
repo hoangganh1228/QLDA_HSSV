afbfbd8e21aaf67349806fb5efb0a2dd875359b1
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-12-29 21:26:47 +0700

    Lưu thay đổi trước khi pull

--- index.php
+++ index.php
@@ -2,10 +2,8 @@
 session_start();
 
 require_once 'app/App.php';
-<<<<<<< HEAD
-=======
+
 require_once 'app/helpers/function.php';
->>>>>>> d2a1f1f15bfcddb260b48e41c5b5b7b115614ed3
 require_once 'core/SqlConnection.php';
 require_once 'core/Controller.php';
 require_once 'core/Database.php';
@@ -13,8 +11,5 @@ require_once 'core/Model.php';
 require_once 'core/Functions.php';
 require_once 'configs/config.php';
 
-<<<<<<< HEAD
-=======
 
->>>>>>> d2a1f1f15bfcddb260b48e41c5b5b7b115614ed3
 $app = new App();
